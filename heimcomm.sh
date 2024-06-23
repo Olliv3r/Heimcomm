653f924c0a4117dd4ef8082fa2d82a63f112bba4
Olliv3r <oliveobom100@gmail.com>
2024-06-23 05:13:56 -0400

    Suporte a extração do arquivo ZIP da stock rom

--- heimcomm.sh
+++ heimcomm.sh
@@ -7,22 +7,12 @@
 partition_names_array=()
 flash_filenames_array=()
 command=("heimdall flash")
+directory_md5=("AP" "BL" "CSC" "CP" "HOME")
 
-check_os() {
-	device=$(getprop ro.product.vendor.brand)
-
-	if [ "$device" != "samsung" ] ; then
-		echo -e "\e[1;31mDispositivo $device não compatível com este programa, tentar executá-lo pode resultar em erros.\e[0m"
-		exit
-	else
-		echo -e "\e[1;32mDispositivo compatível: $device\e[0m"
-	fi
-}
-
+# LER ARQUIVO PIT EXTRAÍDO DO DISPOSITIVO
 read_pit() {
 	pit_file=$1
 
-	# LENDO ARQUIVO PIT EXTRAÍDO DO DISPOSITIVO
 	while IFS= read -r line; do
 		if [[ "$line" == "Partition Name:"* ]]; then
 			partition_name=${line#Partition Name: }
@@ -32,8 +22,10 @@ read_pit() {
 			flash_filename=${line#Flash Filename: }
 
 			if [[ -n "$partition_name" && "$partition_name" != "-" && -n "$flash_filename" && "$flash_filename" != "-" ]]; then
-				partition_names_array+=("$partition_name")
-				flash_filenames_array+=("$flash_filename")
+				if [[ -f extracted/$flash_filename ]]; then
+					partition_names_array+=("$partition_name")
+					flash_filenames_array+=("$flash_filename")
+				fi
 			fi
 
 			partition_name=""
@@ -42,8 +34,8 @@ read_pit() {
 	done < "$pit_file"
 }
 
+# CONSTRUINDO COMANDO LONGO DO HEIMDALL
 build_command() {
-	# CONSTRUINDO COMANDO LONGO DO HEIMDALL
 	if [[ ${#partition_names_array[*]} -eq ${#flash_filenames_array[*]} ]]; then
 		for ((i=0; i < ${#partition_names_array[*]}; i++)); do
 			param="--${partition_names_array[$i]} ${flash_filenames_array[$i]}"
@@ -55,62 +47,68 @@ build_command() {
 	echo -e "${command[*]}\n"
 }
 
-
-extract_all() {
-	for file in ./* ; do
-		if [ ! -d $file ]; then
-			lz4 $file
-		fi
+# EXTRAI TODOS OS ARQUIVO TAR.MD5
+extract_all_tar_md5() {
+	echo -e "\e[1;32mExtraíndo arquivos (MD5)...\e[0m"
+	for dir in ${directory_md5[*]}; do
+		tar -xvf ${dir}_* -C $dir
 	done
-	cd ..
+	rm -rf *.md5
+}
+
+# EXTRAI TODOS OS ARQUIVO LZ4
+extract_all_lz4() {
+	echo -e "\e[1;32mExtraíndo arquivos (LZ4)...\e[0m"
+	for dir in ${directory_md5[*]}; do
+		for file in ./${dir}/* ; do
+			if [ ! -d $file ]; then
+				lz4 $file
+			fi
+		done
+	done
+}
+
+# MOVE TODOS OS ARQUIVO EXTRAIDOS
+move_files_all() {
+	echo -e "\e[1;32mMovendo arquivos...\e[0m"
+	[ ! -d extracted ] && mkdir extracted
+
+	for dir in ${directory_md5[*]}; do
+		mv $dir/* extracted
+	done
 }
 
 extract() {
 	ext=$(echo "$1" | cut -d . -f2)
-	[ "$ext" != "zip" ] && echo "Extensão invalida!" && exit
-	clear
-	file=$1
-	size=$(ls -lh $file | cut -d " " -f 5)
+	[ "$ext" != "zip" ] && echo -e "\e[1;31mExtensão invalida!\e[0m" && exit
 	
-	echo -e "\e[1;32mNome do arquivo: \e[1;36m[$file]\e[0m"
-	echo -e "\e[1;32mTamanho do arquivo: \e[1;36m[$size]\e[0m"
 	echo -e "\e[1;32mExtraíndo arquivo (ZIP)...\e[0m"
 	unzip $file
+	rm $file
 
-	mkdir extracted
+	if [ $? -eq 9 ]; then
+		echo -e "\e[1;31mArquivo ZIP invalido!\e[0m" && exit 9
+	fi
+
 	mkdir AP BL CP CSC HOME
 	
-	echo -e "\e[1;32mExtraíndo arquivos (MD5)...\e[0m"
-	tar -xvf AP_* -C AP
-	tar -xvf BL_* -C BL
-	tar -xvf CP_* -C CP
-	tar -xvf CSC_* -C CSC
-	tar -xvf HOME_* -C HOME
-
-	echo -e "\e[1;32mExtraíndo arquivos (LZ4)...\e[0m"
-	cd AP && extract_all
-	cd BL && extract_all
-	cd CP && extract_all
-	cd CSC && extract_all
-	cd HOME && extract_all
-
-	echo -e "\e[1;32mMovendo todos os extraídos para o um diretório separado...\e[0m"
-	cp AP/* extracted -rf
-	cp BL/* extracted -rf
-	cp CP/* extracted -rf
-	cp CSC/* extracted -rr
-	cp HOME/* extracted -rf
+	extract_all_tar_md5
+	extract_all_lz4
+	move_files_all
 
 	rm -rf AP BL CP CSC HOME *.md5
 	rm -rf extracted/*.lz4
-	echo -e "\n\e[1;31mOs arquivos foram movidos para o diretório 'extracted'.\e[0m"
-	echo -e "\e[1;36mO processo de extração da STOCK ROM finalizado.\e[0m"
+	echo -e "\n\e[1;32mOs arquivos foram movidos para o diretório 'extracted'.\e[0m"
+	echo -e "\e[1;36mProcesso de extração da STOCK ROM finalizado.\e[0m"
+	
 }
 
+# Ajuda
 helper() {
-	echo -e "Usage: $(basename $0) <action> <action arguments>\n\nAction: check-os\nDescription: Verifica se o dispositivoo tem suporte ao uso desse programa.\n\nAction: extrack\nArguments: [FILE_ZIP_STOCK_ROM]\nDescription: Extrai o arquivo da STOCK ROM passando pelo os 4 níveis de compactação.\n\nAction: build-command\nArguments: [FILE_PIT]\nDescription: Constrói comando longo do heimdall de acordo com o arquivo PIT extraído do dispositivo."
+	echo -e "Usage: $(basename $0) <action> <action arguments>\n\nAction: extract\nArguments: [FILE_ZIP_STOCK_ROM]\nDescription: Extrai o arquivo da STOCK ROM passando pelo os 4 níveis de compactação.\n\nAction: build-command\nArguments: [FILE_PIT]\nDescription: Constrói comando longo do heimdall de acordo com o arquivo PIT extraído do dispositivo."
 }
 
+# TRATAMENTO DE OPÇÔES
 if [ -z "$1" ];then
 	helper
 	exit
@@ -118,7 +116,6 @@ else
 	while [ -n "$1" ]; do
 		case "$1" in
 			help) helper;;
-			check-os) check_os;;
 			extract)
 				shift
 				file=$1
